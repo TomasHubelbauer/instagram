ac6d888c3b55f72dc9d0e9cf2e95e4476bb300bd
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-02-12 21:32:18 +0100

    Use new email API, stamp data and log data

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const fetch = require('node-fetch');
 const fs = require('fs-extra');
 let email;
+let headers;
 try {
   email = require('../self-email');
+  headers = require('../self-email/headers');
 }
 catch (error) {
   // Ignore the lack of emailer on the system
@@ -26,6 +28,7 @@ module.exports = async function () {
     /** @typedef {{ followers: number; following: number; posts: nunber; }} Data */
     /** @type {Data} */
     const data = {
+      stamp: new Date().toISOString(),
       bio: user.biography,
       web: user.external_url,
       followers: user.edge_followed_by.count,
@@ -37,27 +40,28 @@ module.exports = async function () {
       posts: user.edge_owner_to_timeline_media.count,
     };
 
+    console.log(handle);
+    console.log('\tfollowers', data.followers);
+    console.log('\tfollowing', data.following);
+    console.log('\tposts', data.posts);
+
     const fileName = handle + '.data.json';
     if (email) {
       try {
         /** @type {Data} */
         const knownData = await fs.readJson(fileName);
-        await email(`
-From: Instagram Bot <bot@hubelbauer.net>
-To: Tomas Hubelbauer <tomas@hubelbauer.net>
-Subject: Instagram Stats for ${handle}
-Content-Type: text/html
-
-<ul>
-<li>Followers: ${followers} (${knownData.followers === data.followers ? 'unchanged' : `from ${knownData.followers}`})</li>
-<li>Following: ${following} (${knownData.following === data.following ? 'unchanged' : `from ${knownData.following}`})</li>
-<il>Posts: ${posts} (${knownData.posts === data.posts ? 'unchanged' : `from ${knownData.posts}`})</li>
-</ul>
-
-Thanks!
-`);
+        await email(
+          headers('Instagram Bot', `Instagram Stats for ${handle}`),
+          '<ul>',
+          `<li>Followers: ${data.followers} (${knownData.followers === data.followers ? 'unchanged' : `from ${knownData.followers}`})</li>`,
+          `<li>Following: ${data.following} (${knownData.following === data.following ? 'unchanged' : `from ${knownData.following}`})</li>`,
+          `<il>Posts: ${data.posts} (${knownData.posts === data.posts ? 'unchanged' : `from ${knownData.posts}`})</li>`,
+          '</ul>',
+          'Thanks!'
+        );
       }
       catch (error) {
+        console.log(error);
         // Ignore the case of no known data being persisted yet
       }
     }
